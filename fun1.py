21c1cde5148694dc93f699bc90163d11ba7a06f4
fathimarinzy <fathimarinzy02@gmail.com>
2024-08-21 21:40:33 +0530

    added function

---
+++ fun1.py
@@ -0,0 +1,104 @@
+def function_name(n):
+    print("success",n)
+function_name(10)
+
+def function_name(n,m):
+    print("success",n,m)
+function_name(10,20)
+
+
+# addition using fun
+def add(a,b):
+    print("sum=",a+b)
+add(10,20)
+
+def add(a,b):
+    print(a+b)
+x=int(input("enter 1 no:"))
+y=int(input("enter 2 no:"))
+add(x,y)
+
+
+#return stmt
+def add(a,b):
+   return a+b
+x=int(input("enter 1 no:"))
+y=int(input("enter 2 no:"))
+print(add(x,y))
+
+# #cannot use return and print stmt
+def add(a,b):
+    return a+b
+    # print a+b
+x=int(input("enter 1 no:"))
+y=int(input("enter 2 no:"))
+print(add(x,y))
+
+
+def fun(n):
+    print("success",n)
+    for i in n:
+        print(i)
+a=[10,20,30]
+fun(a)
+
+
+# #pass the list and finsd the sum
+def fun(n):
+    print("sum=",sum(a))
+a=[10,20,30]
+fun(a)
+
+#pass the string and check palindrome
+
+def fun(n):
+    if n == n[::-1]:
+        print("palindrome",n)
+    else:
+        print(" palindrome",n)
+a = "malayalam"
+fun(a)
+
+# calculator
+
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    return x // y
+
+def calculator():
+    print(" Calculator")
+   
+    while True:
+      choice = input("\n1.add\n2.sub\n3.multiply\n4.div\n5.exit\nEnter your choice : ")
+
+      if choice == "1":
+        a = int(input("Enter first number: "))
+        b= int(input("Enter second number: "))
+        print( add(a,b))
+      elif choice == "2":
+        a = int(input("Enter first number: "))
+        b= int(input("Enter second number: "))
+        print( subtract (a,b))
+      elif choice == "3":
+        a = int(input("Enter first number: "))
+        b= int(input("Enter second number: "))
+        print( multiply(a,b))
+      elif choice == "4":
+        a = int(input("Enter first number: "))
+        b= int(input("Enter second number: "))
+        print(divide(a,b))
+      else:
+        print("exit")
+        break
+
+calculator()
+
+
+# register and login
+
+
--- modules1.py
+++ modules1.py
@@ -1,69 +1,92 @@
-import os
-# os.remove("abc.txt")
-os.rmdir("txxxtis")
+# import os
+# # os.remove("abc.txt")
+# os.rmdir("txxxtis")
 
-#to delete folder with files
-import shutil
-shutil.rmtree("hello")
-with open("abc.txt","w") as file:
-    x=file.write("worked")
-    print(x)
-file.close()
+# #to delete folder with files
+# import shutil
+# shutil.rmtree("hello")
+# with open("abc.txt","w") as file:
+#     x=file.write("worked")
+#     print(x)
+# file.close()
 
 
-import math
-print(math.factorial(5))
-print(math.sqrt(25))
-print(pow(2,5))
+# import math
+# print(math.factorial(5))
+# print(math.sqrt(25))
+# print(pow(2,5))
 
-from math import factorial,sqrt
-print(factorial(25))
-print(sqrt(25))
+# from math import factorial,sqrt
+# print(factorial(25))
+# print(sqrt(25))
 
-from math import *
-print(factorial(25))
-print(sqrt(25))
-print(ceil(1.9))
-print(ceil(1.1))
-print(floor(1.9))
-print(floor(1.1))
-print(pow(2,5))
+# from math import *
+# print(factorial(25))
+# print(sqrt(25))
+# print(ceil(1.9))
+# print(ceil(1.1))
+# print(floor(1.9))
+# print(floor(1.1))
+# print(pow(2,5))
 
-import math
-print(dir(math) )
+# import math
+# print(dir(math) )
 
-import random
-l=[1,2,3,4,5]
-random.shuffle(l)
-print(l)
-print(random.choice(l))
-print(random.random())
-print(random.randint(1,10))
-print(random.randrange(1,10,3))
-print(random.sample(l,3))
+# import random
+# l=[1,2,3,4,5]
+# random.shuffle(l)
+# print(l)
+# print(random.choice(l))
+# print(random.random())
+# print(random.randint(1,10))
+# print(random.randrange(1,10,3))
+# print(random.sample(l,3))
 
-# generate 100 lottery tickets and select two winner
-import random
-s=[random.randint(000000,999999) for i in range(100)]
-print(random.sample(s,2))
+# # generate 100 lottery tickets and select two winner
+# import random
+# s=[random.randint(000000,999999) for i in range(100)]
+# print(random.sample(s,2))
 
-# select 3 no btw 100 to 999 which is divisible by 5
-import random
-for i in range(3):
-    print(random.randrange(100,999,5))
+# # select 3 no btw 100 to 999 which is divisible by 5
+# import random
+# for i in range(3):
+#     print(random.randrange(100,999,5))
 
-#check to find the computer guess and our guess is true
-import random
-com=random.randrange(0,9)
-# print(com)
-guess=0
-while guess<3:
-    n=int(input("enter the no:"))
-    if com==n:
-        print("you win")
-        break
-    else:
-        print("you lose")
-    guess+=1
-else:
-    print("game over")
+# #check to find the computer guess and our guess is true
+# import random
+# com=random.randrange(0,9)
+# # print(com)
+# guess=0
+# while guess<3:
+#     n=int(input("enter the no:"))
+#     if com==n:
+#         print("you win")
+#         break
+#     else:
+#         print("you lose")
+#     guess+=1
+# else:
+#     print("game over")
+
+
+import datetime
+x=datetime.datetime.now()
+print(x)
+print(x.day)
+print(x.month)
+print(x.year)
+print(x.hour)
+print(x.minute)
+print(x.second)
+print(x.microsecond)
+print(x.date())
+print(x.time())
+print(x.strftime("%b"))
+print(x.strftime("%B"))
+print(x.strftime("%w"))
+print(x.strftime("%a"))
+print(x.strftime("%A"))
+print(x.strftime("%y"))
+print(x.strftime("%Y"))
+print(x.strftime("%d"))
+print(x.strftime("%m"))
