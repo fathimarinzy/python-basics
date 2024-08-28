904b16cdb69c8d420420d34198a95751ec127214
fathimarinzy <fathimarinzy02@gmail.com>
2024-08-28 22:23:50 +0530

    inheritance

--- oops1.py
+++ oops1.py
@@ -1,273 +1,274 @@
-#create a class
-class Bird:
-    eyes=2
-    beak=1
-parrot=Bird()
-print(parrot.eyes)
-print(parrot.beak)
+# #create a class
+# class Bird:
+#     eyes=2
+#     beak=1
+# parrot=Bird()
+# print(parrot.eyes)
+# print(parrot.beak)
 
-# mul objects with one class
-class Bird:
-    eyes=2
-    beak=1
-parrot=Bird()
-print(parrot.eyes)
-print(parrot.beak)
+# # mul objects with one class
+# class Bird:
+#     eyes=2
+#     beak=1
+# parrot=Bird()
+# print(parrot.eyes)
+# print(parrot.beak)
 
-peacock=Bird()
-print(peacock.eyes)
-print(peacock.beak)
+# peacock=Bird()
+# print(peacock.eyes)
+# print(peacock.beak)
 
-class Bird:
-    eyes=2
-    beak=1
-parrot=Bird()
-print(parrot.beak)
-parrot.eyes=1
-print(parrot.eyes,"eyes")
-peacock=Bird()
-print(peacock.eyes)
-print(peacock.beak)
-
-# give a method
-class Bird:
-    eyes=2
-    beak=1
-    def fly(self):
-        print("parrot")
-parrot=Bird()
-print(parrot.beak)
-parrot.eyes=1
-print(parrot.eyes,"eyes")
-parrot.fly()
-peacock=Bird()
-print(peacock.eyes)
-print(peacock.beak)
-peacock.fly()
-
-class Bird:
-    eyes=2
-    beak=1
-    def fly(self,a):
-     print(a)
-parrot=Bird()
+# class Bird:
+#     eyes=2
+#     beak=1
+# parrot=Bird()
 # print(parrot.beak)
 # parrot.eyes=1
 # print(parrot.eyes,"eyes")
-parrot.fly("parrot")
-peacock=Bird()
-print(peacock.eyes)
-print(peacock.beak)
-peacock.fly("peacock")
+# peacock=Bird()
+# print(peacock.eyes)
+# print(peacock.beak)
 
-   #use of instance variable
-class Bird:
-    eyes=2
-    beak=1
-    def fly(self,a):
-       self.a=a
-       print(a)
-    def walk(self):
-        print(self.a)
+# # give a method
+# class Bird:
+#     eyes=2
+#     beak=1
+#     def fly(self):
+#         print("parrot")
+# parrot=Bird()
+# print(parrot.beak)
+# parrot.eyes=1
+# print(parrot.eyes,"eyes")
+# parrot.fly()
+# peacock=Bird()
+# print(peacock.eyes)
+# print(peacock.beak)
+# peacock.fly()
+
+# class Bird:
+#     eyes=2
+#     beak=1
+#     def fly(self,a):
+#      print(a)
+# parrot=Bird()
+# # print(parrot.beak)
+# # parrot.eyes=1
+# # print(parrot.eyes,"eyes")
+# parrot.fly("parrot")
+# peacock=Bird()
+# print(peacock.eyes)
+# print(peacock.beak)
+# peacock.fly("peacock")
+
+#    #use of instance variable
+# class Bird:
+#     eyes=2
+#     beak=1
+#     def fly(self,a):
+#        self.a=a
+#        print(a)
+#     def walk(self):
+#         print(self.a)
 
 
-parrot=Bird()
+# parrot=Bird()
 
-parrot.fly("parrot")
-parrot.walk()
-parrot.eyes=1
-peacock=Bird()
-peacock.fly("peacock")
+# parrot.fly("parrot")
+# parrot.walk()
+# parrot.eyes=1
+# peacock=Bird()
+# peacock.fly("peacock")
 
 
-#create a 3 methods
+# #create a 3 methods
 
-class Add:
-    def num(self,a,b):
-        self.a=a
-        self.b=b
-        print(b)
-    def calc(self):
+# class Add:
+#     def num(self,a,b):
+#         self.a=a
+#         self.b=b
+#         print(b)
+#     def calc(self):
        
-        self.c=self.a+self.b
-    def res(self):
-        print(self.c)
-add=Add()
-add.num(10,20)
-add.calc()
-add.res()
+#         self.c=self.a+self.b
+#     def res(self):
+#         print(self.c)
+# add=Add()
+# add.num(10,20)
+# add.calc()
+# add.res()
 
-#3 methods class=rectangle calculate area and perimeter
+# #3 methods class=rectangle calculate area and perimeter
 
-class Rectangle:
-    def val(self,a,b):
-        self.a=a
-        self.b=b
-    def area(self):
-        self.c=self.a*self.b
-        print(self.c)
-    def perimeter(self):
-        self.d=2*self.a*self.b
-        print(self.d)
-rec=Rectangle()
-rec.val(2,4)
-rec.area()
-rec.perimeter()
+# class Rectangle:
+#     def val(self,a,b):
+#         self.a=a
+#         self.b=b
+#     def area(self):
+#         self.c=self.a*self.b
+#         print(self.c)
+#     def perimeter(self):
+#         self.d=2*self.a*self.b
+#         print(self.d)
+# rec=Rectangle()
+# rec.val(2,4)
+# rec.area()
+# rec.perimeter()
 
-#3 method class=book display tilte and author
-class Book:
-    def tit(self,a):
-        self.a=a
-    def aut(self,b):
-        self.b=b
-    def display(self):
-        # print(self.a + self.b)
-        print(self.a)
-        print(self.b) 
-boo=Book()
-boo.tit("Adujeevitham")
-boo.aut("Benyamin")
-boo.display()
+# #3 method class=book display tilte and author
+# class Book:
+#     def tit(self,a):
+#         self.a=a
+#     def aut(self,b):
+#         self.b=b
+#     def display(self):
+#         # print(self.a + self.b)
+#         print(self.a)
+#         print(self.b) 
+# boo=Book()
+# boo.tit("Adujeevitham")
+# boo.aut("Benyamin")
+# boo.display()
 
-#class=bank 4 methods
-class Bank():
+# #class=bank 4 methods
+# class Bank():
          
-    def initialamount(self,initialamount):
-        self.balance=initialamount
+#     def initialamount(self,initialamount):
+#         self.balance=initialamount
       
-    def deposit(self,deposit):
+#     def deposit(self,deposit):
        
-        self.balance+=deposit
-        print("current balance",self.balance)
+#         self.balance+=deposit
+#         print("current balance",self.balance)
        
-    def withdraw(self,withdraw):
-        if withdraw> self.balance:
-            print("Insufficient balance!")
-        else:
-            self.balance -= withdraw
-            print("Current balance:", self.balance)
+#     def withdraw(self,withdraw):
+#         if withdraw> self.balance:
+#             print("Insufficient balance!")
+#         else:
+#             self.balance -= withdraw
+#             print("Current balance:", self.balance)
         
-    def currentbalance(self):
-        print("current balance=",self.balance)
-bank=Bank()      
-while True:
+#     def currentbalance(self):
+#         print("current balance=",self.balance)
+# bank=Bank()      
+# while True:
            
-            choice=int(input("\n1.Add details\n2.Deposit\n3.Withdraw\n4.Current Balance\n5.Exit\nEnter your choice="))
-            if choice==1:
-                while True:
+#             choice=int(input("\n1.Add details\n2.Deposit\n3.Withdraw\n4.Current Balance\n5.Exit\nEnter your choice="))
+#             if choice==1:
+#                 while True:
           
-                    name=input("enter your name:")
-                    acc=int(input("enter your acc no:"))
-                    amount=int(input("Enter your initial balance :"))
-                    bank.initialamount(amount)
-                    break 
-            elif choice==2:
-                depo=int(input("Enter your deposit amount:"))
-                bank.deposit(depo)
+#                     name=input("enter your name:")
+#                     acc=int(input("enter your acc no:"))
+#                     amount=int(input("Enter your initial balance :"))
+#                     bank.initialamount(amount)
+#                     break 
+#             elif choice==2:
+#                 depo=int(input("Enter your deposit amount:"))
+#                 bank.deposit(depo)
                 
-            elif choice==3:
-                n=int(input("Enter your withdraw amount:"))
-                bank.withdraw(n)
-            elif choice==4:
-                bank.currentbalance() 
-            elif choice==5:
-                print("Exit")
-                break
+#             elif choice==3:
+#                 n=int(input("Enter your withdraw amount:"))
+#                 bank.withdraw(n)
+#             elif choice==4:
+#                 bank.currentbalance() 
+#             elif choice==5:
+#                 print("Exit")
+#                 break
 
 
-#contruct
-class student:
-    college="abc"
-    def __init__(self,a):
-        print("welcome",a)
-    def study(self):
-        print("studying")
-ammu=student(1000)       
-print(ammu.college)
-ammu.study()
+# #contruct
+# class student:
+#     college="abc"
+#     def __init__(self,a):
+#         print("welcome",a)
+#     def study(self):
+#         print("studying")
+# ammu=student(1000)       
+# print(ammu.college)
+# ammu.study()
 
 
-#create class person that initialize with name and age with include a method
-# displAY info that prints the name and age of tht person
-class Person:
+# #create class person that initialize with name and age with include a method
+# # displAY info that prints the name and age of tht person
+# class Person:
     
-    def __init__(self,name,age) :
-        self.name=name
-        self.age=age
-    def display(self):
-        print("name",self.name)
-        print("name",self.age)
-p=Person("rinzy",19)
-p.display()
-# #
-class Rectangle:
-    def __init__(self,length,breadth):
-        self.length=length
-        self.breadth=breadth
-    def area(self):
-        print("area=",self.length*self.breadth)
-p=Rectangle(10,30)
-p.area()
+#     def __init__(self,name,age) :
+#         self.name=name
+#         self.age=age
+#     def display(self):
+#         print("name",self.name)
+#         print("name",self.age)
+# p=Person("rinzy",19)
+# p.display()
+# # #
+# class Rectangle:
+#     def __init__(self,length,breadth):
+#         self.length=length
+#         self.breadth=breadth
+#     def area(self):
+#         print("area=",self.length*self.breadth)
+# p=Rectangle(10,30)
+# p.area()
 
-# #
-class Student:
-    def __init__(self,name,grade):
+# # #
+# class Student:
+#     def __init__(self,name,grade):
         
-        self.name=name
-        self.grade=grade
-    def average(self):
-       self.avg=sum(self.grade)//len(self.grade)
-    def display(self):
-        print("name=",self.name)
-        print("grade=",self.grade)
-        print("average=",self.avg)
-p=Student("rinzy",[85,90,78,92])
-p.average()
+#         self.name=name
+#         self.grade=grade
+#     def average(self):
+#        self.avg=sum(self.grade)//len(self.grade)
+#     def display(self):
+#         print("name=",self.name)
+#         print("grade=",self.grade)
+#         print("average=",self.avg)
+# p=Student("rinzy",[85,90,78,92])
+# p.average()
 
-#inheritance
-class Father:
-    def phone(self):
-        print("phone is using")
-    def walk(self):
-        print("walking")
-class Son(Father):
+# #inheritance
+##single inheritance
+# class Father:
+#     def phone(self):
+#         print("phone is using")
+#     def walk(self):
+#         print("walking")
+# class Son(Father):
   
-        print("studying")
-f=Son()
-f.phone()
-f.walk()
+#         print("studying")
+# f=Son()
+# f.phone()
+# f.walk()
 
-class Father:
-    def phone(self):
-        print("phone is using")
-    def walk(self):
-        print("walking")
-class Son(Father):
-    def study(self):
-        print("studying")
-f=Son()
-f.phone()
-f.walk()
-f.study()
+# class Father:
+#     def phone(self):
+#         print("phone is using")
+#     def walk(self):
+#         print("walking")
+# class Son(Father):
+#     def study(self):
+#         print("studying")
+# f=Son()
+# f.phone()
+# f.walk()
+# f.study()
 
-#inheritance with constructor
-class Father:
-    def __init__(self):
-        print("father page")
-    def phone(self):
-        print("phone is using")
-    def walk(self):
-        print("walking")
-class Son(Father):
-    def __init__(self):
-        print("son page")
-    def study(self):
-        print("studying")
-f=Son()
-f.phone()
-f.walk()
-f.study()
-#super method
+# #inheritance with constructor
+# class Father:
+#     def __init__(self):
+#         print("father page")
+#     def phone(self):
+#         print("phone is using")
+#     def walk(self):
+#         print("walking")
+# class Son(Father):
+#     def __init__(self):
+#         print("son page")
+#     def study(self):
+#         print("studying")
+# f=Son()
+# f.phone()
+# f.walk()
+# f.study()
+# #super method
 class Father:
     def __init__(self,a):
         print("father page",a)
@@ -278,13 +279,116 @@ class Father:
 class Son(Father):
     def __init__(self,s,f):
         print("son page")
-        super.__init__(f)#another method
+        super().__init__(f)#another method
         # Father.__init__(self)
     def study(self):
         print("studying")
-f=Son()
+f=Son("son","father")
 f.phone()
 f.walk()
-f.study()
 
+
+
+#multilevel inheritance
+class Grandfather :
+    def phone(self):
+        print("phone is using")
+class Father(Grandfather):
+    def walk(self):
+        print("walking")
+class Son(Father):
+    def study(self):
+        print("studying")
+f=Son()
+f.study()
+f.phone()
+f.walk()
+
+#multiple inheritance
+class Father:
+    def phone(self):
+        print("phone is using")
+class Mother :
+    def  walk(self):
+        print("walking")
+class Son(Mother,Father):
+    def study(self):
+        print("studying")
+f=Son()
+f.study()
+f.phone()
+f.walk()
+# same method in parent 
+class Father:
+    def walk(self):
+        print("phone is using")
+class Mother :
+    def  walk(self):
+        print("walking")
+class Son(Mother,Father):
+    def study(self):
+        print("studying")
+f=Son()
+f.study()
+f.walk()
+
+# #hierarchial inheritance
+class Vehicle:
+    def acceleration(self):
+        print("vehicle class")
+class Car(Vehicle):
+    def four(self):
+        print("four wheeler")
+class Bike(Vehicle):
+    def two(self):
+        print("two wheeler")
+f=Car()
+f.four()
+f.acceleration()
+# f.two() #car has no method two .car and bike has no relation
+ff=Bike()
+# ff.four() #bike has no method four .car and bike has no relation
+ff.acceleration()
+ff.two()
+
+#hybrid inheritance
+class Grandfather :
+    def phone(self):
+        print("phone is using") #single
+class Father(Grandfather):
+    def walk(self):
+        print("walking")
+class Son(Grandfather):
+    def Study(self):
+        print("studying")
+class Grandson(Father,Son):#multiple
+    def Eat(self):
+        print("eating")
+obj=Grandson()
+obj.Eat()
+obj.Study()
+obj.walk()
+obj.phone()
+print(Grandson.__mro__)
+
+# constructor in multilevel
+class Grandfather:
+    def __init__(self,g):
+        print("grandfather page")
+    def phone(self):
+        print("phone is using")
+class Father(Grandfather):
+    def __init__(self,f,g):
+        print("father page")
+        super().__init__(g)
+    def walk(self):
+        print("walking")
+class Son(Father):
+    def __init__(self,s,f,g):
+        print("son page")
+        super().__init__(f,g)
+    def study(self):
+        print("studying")
+obj=Son('s','f','g')
+obj.study()
 
